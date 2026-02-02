b1885b874f26a95ab55992c4182361e8a6ff4e80
DGGamer98 <davidegatta03@gmail.com>
2026-02-02 14:53:17 +0100

    Finito il primo junitTest

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -18,25 +18,25 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author david
  */
 public class TestClient {
-    
+
     ClienteDTO clienteDTO;
     ServiceForCliente service;
-    
+
     @BeforeAll
     public static void setUpClass() {
         System.out.println("Stared");
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
         clienteDTO = new ClienteDTO();
         service = new ServiceForCliente();
     }
-    
+
     @AfterEach
     public void tearDown() {
         System.out.println("Stop");
@@ -51,21 +51,104 @@ public class TestClient {
         clienteDTO.setNome("Davide");
         clienteDTO.setCognome("Gatta");
         clienteDTO.setEta(23);
-        
+
         service.createClient(clienteDTO);
         service.findAll();
-       
+
         ClienteDTO risultato = service.findById(1);
-        
+
         Assertions.assertNotNull(risultato);
-        
+
         Assertions.assertEquals(risultato.getId(), clienteDTO.getId());
         Assertions.assertEquals(risultato.getNome(), clienteDTO.getNome());
         Assertions.assertEquals(risultato.getCognome(), clienteDTO.getCognome());
         Assertions.assertEquals(risultato.getEta(), clienteDTO.getEta());
-        
 
         service.controlAge(clienteDTO);
-  
+
+    }
+
+    @Test
+    public void findById() {
+        clienteDTO.setId(1);
+        clienteDTO.setNome("Davide");
+        clienteDTO.setCognome("Gatta");
+        clienteDTO.setEta(23);
+
+        service.createClient(clienteDTO);
+
+        ClienteDTO risultato = service.findById(1);
+
+    }
+
+    @Test
+    public void findAll() {
+        ClienteDTO client = new ClienteDTO();
+
+        clienteDTO.setId(1);
+        clienteDTO.setNome("Davide");
+        clienteDTO.setCognome("Gatta");
+        clienteDTO.setEta(23);
+
+        client.setId(2);
+        client.setNome("Giacomo");
+        client.setCognome("Rossi");
+        client.setEta(20);
+
+        service.createClient(client);
+        service.createClient(clienteDTO);
+
+        service.findAll();
+
+        Assertions.assertNotNull(client);
+        Assertions.assertNotNull(clienteDTO);
+
+    }
+
+    @Test
+    public void update() {
+        ClienteDTO client = new ClienteDTO();
+
+        clienteDTO.setId(1);
+        clienteDTO.setNome("Davide");
+        clienteDTO.setCognome("Gatta");
+        clienteDTO.setEta(23);
+
+        client.setId(2);
+        client.setNome("Giacomo");
+        client.setCognome("Rossi");
+        client.setEta(20);
+
+        service.createClient(client);
+        service.createClient(clienteDTO);
+
+        service.update(client);
+
+    }
+
+    @Test
+    public void delete() {
+        ClienteDTO client = new ClienteDTO();
+
+        clienteDTO.setId(1);
+        clienteDTO.setNome("Davide");
+        clienteDTO.setCognome("Gatta");
+        clienteDTO.setEta(23);
+
+        client.setId(2);
+        client.setNome("Giacomo");
+        client.setCognome("Rossi");
+        client.setEta(20);
+
+        service.createClient(client);
+        service.createClient(clienteDTO);
+        
+        service.delete(1);
+        service.findAll();
+        
+        Assertions.assertNotNull(client);
+        
+        
+
     }
 }
